bd9f7d3f85a8412cfaf8fa786f8b01569395bde5
Johnny Chen <78446260+chenjohnny-cs@users.noreply.github.com>
2025-10-31 16:33:30 -0400

    Updated docker_runner.py Script
    docker_runner.py is update to now read average memory and cpu readings overtime instead of reading it at the start and end.
    
    Added more test files and ability to choose which test file to run when using the docker_runner.py script.

--- scripts/docker_runner.py
+++ scripts/docker_runner.py
@@ -1,57 +1,22 @@
 import docker
 import time
 import psutil
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 import tqdm
 
 ## This script will allow us to run a specific docker container with specific cpu and memory limits
 ## for a specific type of code.
 
 ## Need to figure out file uploads, etc.
-
 client = docker.from_env()
 
+def f(container_id, res_dict):
+    container = client.containers.get(container_id=container_id)
 
-def f(container_id):
-    with tqdm.tqdm(total=100, desc='CPU %', position=0) as cpubar, tqdm.tqdm(total=100, desc='RAM %', position=1) as rambar:
+    cpu_readings = []
+    mem_readings = []
 
-        container = client.containers.get(container_id=container_id)
-        while True:
-            stats = container.stats(stream = False)
-            
-            cpu_delta = stats["cpu_stats"]["cpu_usage"]["total_usage"] - stats["precpu_stats"]["cpu_usage"]["total_usage"]
-            system_delta = stats["cpu_stats"]["system_cpu_usage"] - stats["precpu_stats"]["system_cpu_usage"]
-            num_cpus = stats["cpu_stats"]["online_cpus"]
-
-            cpu_percent = (cpu_delta / system_delta) * num_cpus * 100.0 if system_delta > 0 else 0.0
-
-            # Memory usage
-            mem_usage = stats["memory_stats"]["usage"]
-            mem_limit = stats["memory_stats"]["limit"]
-            mem_percent = (mem_usage / mem_limit) * 100.0
-
-            rambar.n=mem_percent
-            cpubar.n=cpu_percent
-
-            rambar.refresh()
-            cpubar.refresh()
-            time.sleep(0.5)
-
-if __name__ == '__main__':
-    try:
-        container = client.containers.run(
-            image="chenjohnnycs/python-script-runner",
-            cpu_count=2,
-            mem_limit="12mb", # Memory Limit is 6 megabytes
-            detach=True
-        )
-
-        print("Container {} started.".format(container.short_id))
-
-        ## Start Live Ram / CPU bars
-        # p = Process(target=f, args=(container.id,))
-        # p.start()
-
+    while res_dict["running"]:
         stats = container.stats(stream = False)
             
         cpu_delta = stats["cpu_stats"]["cpu_usage"]["total_usage"] - stats["precpu_stats"]["cpu_usage"]["total_usage"]
@@ -65,12 +30,47 @@ if __name__ == '__main__':
         mem_limit = stats["memory_stats"]["limit"]
         mem_percent = (mem_usage / mem_limit) * 100.0
 
-        print("CPU Percentage:",cpu_percent)
-        print("RAM Percentage:",mem_percent)
+        cpu_readings.append(cpu_percent)
+        mem_readings.append(mem_readings)
 
-        # ## Running specific test file
-        print("Running test file main.py:")
-        container.exec_run("python -u test_files/main.py")
+        time.sleep(0.25)
+
+    res_dict["avg_cpu"] = sum(cpu_readings) / len(cpu_readings) if cpu_readings else 0
+    res_dict["avg_mem"] = sum(mem_readings) / len(mem_readings) if mem_readings else 0
+
+if __name__ == '__main__':
+    try:
+        file = input("Enter the file name you want to run: ").strip()
+
+        container = client.containers.run(
+            image="chenjohnnycs/python-script-runner",
+            cpu_count=2,
+            mem_limit="12mb", # Memory Limit is 6 megabytes
+            detach=True
+        )
+        print("Container {} started.".format(container.short_id))
+
+        with Manager() as manager:
+            res_dict = manager.dict()
+            res_dict["running"] = True
+
+            p = Process(target=f, args=(container.id, res_dict))
+            p.start()
+
+            ## Running specific test file
+            print("Running test file {}:".format(file))
+            for chunk in container.exec_run("python -u test_files/{}".format(file), stream=True).output:
+                print(chunk.decode())
+
+            res_dict["running"] = False
+            p.join()
+
+            print("==== Average Running Stats ====")
+            print("Average CPU stats --> {}".format(res_dict["avg_cpu"]))
+            print("Average MEM stats --> {}".format(res_dict["avg_mem"]))
+
+        container.stop()
+        container.remove()
 
     except docker.errors.ContainerError as E:
         print("Error running Docker Container", E)
--- test_files/mem.py
+++ test_files/mem.py
@@ -3,5 +3,6 @@ import time
 if __name__ == '__main__':
     print("Running Docker from Python")
 
-    x = [0 for _ in range(5000000)]
+    x = [0 for _ in range(50000)]
+    print("Completed allocation task.")
     time.sleep(4)
